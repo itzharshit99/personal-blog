2247f418cf559ad5760e1175abc1ff9aab443ef6
Harshit Jain <iharshitjain99@gmail.com>
2025-01-04 22:48:10 +0530

    related blog functionality implemented

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -116,4 +116,31 @@ router.delete("/:id",async(req,res)=>{
     res.status(500).send({message:"Error deleting post"})
   }
 })
+
+//get related blogs
+router.get("/related/:id",async(req,res)=>{
+  try {
+    const {id} = req.params;
+    if(!id){
+      return res.status(400).send({message:"post id is required"})
+    }
+    const blog = await Blog.findById(id);
+    if(!blog){
+      return res.status(404).send({message:"post not found"})
+    }
+    const titleRegex = new RegExp(blog.title.split(" ").join('|'),'i');
+    const relatedQuery={
+      _id:{$ne:id},//exclude current blog
+      title:{$regex:titleRegex}
+    }
+    const relatedPost = await Blog.find(relatedQuery);
+    res.status(200).send({message:"related post found",post:relatedPost})
+
+
+    
+  } catch (error) {
+    console.error("Error fetching in related post",error);
+    res.status(500).send({message:"Error fetching related post"})
+  }
+})
 module.exports = router;
