3c662d61c2393d65b0363b92fa09dca7dd5643ac
Harshit Jain <iharshitjain99@gmail.com>
2025-01-04 11:52:16 +0530

    implemented create-post route and saving in db

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const Blog = require("../models/blog.model.js");
 const router = express.Router();
+//get all blogs
 router.get("/", (req, res) => {
   res.send("blog routes");
 });
+router.post("/create-post", async (req,res) => {
+  try {
+    //all data comming in res --> console.log(req.body);
+    //saving data in db
+    const newPost = new Blog({...req.body});
+    await newPost.save();
+    res.status(201).send({
+      message:"post created successfully",
+      post:newPost
+    })
 
+  } 
+  catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).send({ message: "error creating post" });
+  }
+});
 module.exports = router;
