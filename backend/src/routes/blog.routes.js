68f59ffdfbac8159bcb3f4d10341032b83ff95df
Harshit Jain <iharshitjain99@gmail.com>
2025-01-04 22:23:50 +0530

    single blog fetched perfectly

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", async(req, res) => {
   try {
     //search functionality
-    const {search} = req.query;
+    const {search,category,location} = req.query;
     console.log(search);
     let query = {}
     if(search){
@@ -15,10 +15,22 @@ router.get("/", async(req, res) => {
           {title:{$regex:search,$options:"i"}},
           {content:{$regex:search,$options:"i"}},
         ]
+      } 
+    }
+    if(category){
+      query={
+        ...query,
+        category:category
+      }
+    }
+    if(location){
+      query={
+        ...query,
+        location:location
       }
     }
     //all blog functionality
-    const post = await Blog.find(query);
+    const post = await Blog.find(query).sort({createdAt:-1});
     res.status(200).send({
       message:"All post fetched",
       posts:post
@@ -47,4 +59,23 @@ router.post("/create-post", async (req,res) => {
     res.status(500).send({ message: "error creating post" });
   }
 });
+//get single blog by id
+router.get("/:id",async(req,res)=>{
+  try {
+    // console.log(req.params.id);
+    const postId = req.params.id;
+    const post = await Blog.findById(postId);
+    if(!post){
+      return res.status(404).send({message:"Post not found"})
+    }
+    res.status(200).send({
+      message:"Post retrieved Successfully",
+      post:post
+    })
+  } catch (error) {
+    console.error("Error fetching in single post",error);
+    res.status(500).send({message:"Error fetching single post"})
+    
+  }
+})
 module.exports = router;
