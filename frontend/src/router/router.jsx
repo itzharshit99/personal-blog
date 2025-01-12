88357c3cbeb2b2c54af7515ab1d282fd9c23e49b
Harshit Jain <iharshitjain99@gmail.com>
2025-01-12 21:25:32 +0530

    pages added

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
+import About from "../pages/About.jsx";
+import ContactUs from "../pages/ContactUs.jsx";
+import PrivacyPolicy from "../pages/PrivacyPolicy.jsx";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,18 @@ const router = createBrowserRouter([
       {
         path:"/",
         element:<Home/>
+      },
+      {
+        path:"/about-us",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<ContactUs/>
+      },
+      {
+        path:"/privacy-policy",
+        element:<PrivacyPolicy/>
       }
     ]
   }
