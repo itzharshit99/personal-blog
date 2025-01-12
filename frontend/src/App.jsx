4ca3ec85f4022c8e573bb537e3932b2077d36476
Harshit Jain <iharshitjain99@gmail.com>
2025-01-12 21:11:40 +0530

    minor issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
-
 const App = () => {
   return (
     <>
@@ -13,7 +12,6 @@ const App = () => {
       </div>
       <footer className='mt-auto'>Footer</footer>
     </div>
-    
     </>
   )
 }
