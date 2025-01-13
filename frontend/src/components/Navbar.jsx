8be753ed2274da175bbd92ca4f804b4444ea510c
Harshit Jain <iharshitjain99@gmail.com>
2025-01-13 17:30:38 +0530

    mobile responsive navbar setup done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "../pages/Home";
 import { NavLink } from "react-router-dom";
+import {IoClose, IoMenuSharp} from 'react-icons/io5';
 const navList = [
   { name: "Home", path: "/" },
   { name: "About us", path: "/about-us" },
@@ -8,6 +9,8 @@ const navList = [
   { name: "contact", path: "/contact" },
 ];
 const Navbar = () => {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+  const toggleMenu = ()=> setisMenuOpen(!isMenuOpen);
   return (
     <header className="bg-white py-6 border">
       <nav className="container mx-auto flex justify-between px-5">
@@ -29,8 +32,40 @@ const Navbar = () => {
               </NavLink>
             </li>
           ))}
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
         </ul>
+        <div className="flex items-center sm:hidden">
+          <button onClick={toggleMenu} className="flex items-center px-3 py-4 bg-[#fafafa] rounded text-sm text-gray-500 hover:text-gray-900">{
+            isMenuOpen ?<IoClose className="size-6"/> :<IoMenuSharp className="size-6"/>
+          }</button>
+        </div>
       </nav>
+      {
+        isMenuOpen && (
+          <ul className="fixed top-[108px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50">
+          {navList.map((list, index) => (
+            <li className="mt-5 px-4">
+              <NavLink
+                onClick={()=>setisMenuOpen(false)}
+                to={`${list.path}`}
+                className={({ isActive}) =>
+                  [
+                    isActive ? "active" : ""
+                  ]
+                }
+              >
+                {list.name}
+              </NavLink>
+            </li>
+          ))}
+          <li className="px-4 mt-5">
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+        )
+      }
     </header>
   );
 };
