a18586b23d9a92d0a678932fd450db6836d37267
Harshit Jain <iharshitjain99@gmail.com>
2025-01-13 18:14:39 +0530

    search functionality implemented

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import SearchBlog from './SearchBlog'
+import React from "react";
+import SearchBlog from "./SearchBlog";
+import { useState } from "react";
 
 const Blogs = () => {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+  const [query, setQuery] = useState({ search: "", category: "" });
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleSearch = () => setQuery({ search, category });
   return (
-    <div className='mt-16 container mx-auto'>
-      <SearchBlog/>
+    <div className="mt-16 container mx-auto">
+      <SearchBlog
+        search={search}
+        handleSearchChange={handleSearchChange}
+        handleSearch={handleSearchChange}
+      />
       <div>blog card</div>
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
--- frontend/src/pages/SearchBlog.jsx
+++ frontend/src/pages/SearchBlog.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const SearchBlog = () => {
+const SearchBlog = (search,handleSearchChange,handleSearch) => {
+  const handleKeyPress =(event)=>{
+    if(event.key=="Enter"){
+      handleSearch();
+    }
+  }
   return (
     <div className='w-full flex'>
-      <input type="text" placeholder='Search for the Blog' className='py-2 px-4 mr-5 w-full bg-[#f7f8f9] focus:outline-none focus:border' />
+      <input value={search} onChange={handleSearchChange} onKeyPress={handleKeyPress} type="text" placeholder='Search for the Blog' className='py-2 px-4 mr-5 w-full bg-[#f7f8f9] focus:outline-none focus:border' />
       <button className='bg-[#1E73BE] px-4 py-2 text-white'>Search</button>
     </div>
   )
