ac8614d76e995341cfb2060936efacf1f475fe38
Harshit Jain <iharshitjain99@gmail.com>
2025-01-15 00:22:29 +0530

    redux implemented

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { blogApi } from './features/blogs/blogsApi'
+export const store = configureStore({
+  reducer:{
+    [blogApi.reducerPath]:blogApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(blogApi.middleware),
+})
