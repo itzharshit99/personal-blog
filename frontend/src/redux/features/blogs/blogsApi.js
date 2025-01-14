922136a79bc6c3e154cdb07442863ed025f77f15
Harshit Jain <iharshitjain99@gmail.com>
2025-01-15 00:14:03 +0530

    blogs api with redux setup done

---
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -0,0 +1,23 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { build } from 'vite'
+
+// Define a service using a base URL and expected endpoints
+export const blogApi = createApi({
+  reducerPath:'blogsApi',
+  baseQuery:fetchBaseQuery({
+    baseUrl:'http://localhost:5000/api/',
+    credentials:'include'
+  }),
+  endpoints:(builder)=>(
+    {
+      fetchBlogs:builder.query({
+        query:({search='',category='',location=''})=> `/blogs?search=${search}&category=${category}&location=${location}`
+      })
+    }
+  )
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useFetchBlogsQuery } = blogApi
