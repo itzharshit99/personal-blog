2f2fd9e287b27c4c6dd2cf1b8c132cbb5ae234b5
Harshit Jain <iharshitjain99@gmail.com>
2025-01-19 15:35:32 +0530

    more updates

---
+++ frontend/src/pages/CommentCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import commentorIcon from "../assets/commentor.png";
+import { formateDate } from '../utils/formateDate';
+const CommentCard = ({comments}) => {
+  return (
+    <div className='my-6 bg-white p-8'>
+      <div>
+        {comments?.length>0 ? <div>
+          <h1 className='text-lg font-medium'>All Comments</h1>
+          <div>
+            {
+              comments.map((comment,index)=>(
+                <div key={index}>
+                  <div>
+                    <img src={commentorIcon} alt="" className='h-14' />
+                    <div>
+                      <p className='text-lg font-medium underline capitalize underline-offset-4  text-blue-400'>{comment?.user.username}</p>
+                      <p className='text-[12px] italic'>{formateDate(comment.createdAt)}</p>
+                    </div>
+                  </div>
+                  <div className='text-gray-600 mt-5 border p-8'>
+                    <p className='md:w-4/5'>{comment?.comment}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div> : <div className='text-lg font-medium'>No comments Found</div>}
+      </div>
+
+      
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CommentCard
