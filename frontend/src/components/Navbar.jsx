630bc01bf1cdc5e94918cfde24771ef8d7395420
Harshit Jain <iharshitjain99@gmail.com>
2025-01-23 21:44:50 +0530

    navbar updation done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Home from "../pages/Home";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { IoClose, IoMenuSharp } from "react-icons/io5";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import avatarImg from "../assets/commentor.png"
+import { useLogoutUserMutation } from "../redux/features/auth/authApi";
+import { logout } from "../redux/features/auth/authSlice";
 const navList = [
   { name: "Home", path: "/" },
   { name: "About us", path: "/about-us" },
@@ -17,6 +20,17 @@ const Navbar = () => {
     "Auth State:",
     useSelector((state) => state.auth)
   );
+  const dispatch = useDispatch();
+  const [logoutUser] = useLogoutUserMutation();
+  const handleLogout = async ()=>{
+    try {
+      await logoutUser().unwrap();
+      dispatch(logout());
+
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <header className="bg-white py-6 border">
@@ -35,9 +49,27 @@ const Navbar = () => {
               </NavLink>
             </li>
           ))}
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
+          {
+            user && user.role === "user" ? (
+              <li className="flex items-center gap-3">
+                <img src={avatarImg} alt="" className="size-8" />
+                <button onClick={handleLogout} className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm">Logout</button>
+              </li>
+
+            ) : (
+              <NavLink to="/login">Login</NavLink>
+
+            )
+          }
+          {
+            user && user.role === "admin" && (
+              <li className="flex items-center gap-3">
+                <img src={avatarImg} alt="" className="size-8" />
+                <Link to="/dashboard" ><button className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm">Dashboard</button></Link>
+              </li>
+
+            )
+          }
         </ul>
         <div className="flex items-center sm:hidden">
           <button
