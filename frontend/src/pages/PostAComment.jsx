491be167b988ad28c08ddbee776bd307dcf08b37
Harshit Jain <iharshitjain99@gmail.com>
2025-01-28 17:56:18 +0530

    post comment issue fixed

--- frontend/src/pages/PostAComment.jsx
+++ frontend/src/pages/PostAComment.jsx
@@ -1,12 +1,45 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { usePostCommentMutation } from "../redux/features/comments/commentApi";
+import { useFetchBlogByIdQuery } from "../redux/features/blogs/blogsApi";
+
 const PostAComment = () => {
   const { id } = useParams();
-  const { comment, setComment } = useState();
+  const [comment, setComment] = useState(""); // Corrected useState usage
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const [postComment] = usePostCommentMutation();
+  const {refetch} = useFetchBlogByIdQuery(id,{skip:!id});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!user) {
+      alert("Please login to comment on this post");
+      navigate("/login");
+      return;
+    }
+    const newComment = {
+      comment: comment,
+      user: user?.id,
+      postId: id,
+    };
+    try {
+      const response = await postComment(newComment).unwrap();
+      console.log("comment added successfully");
+      setComment('');
+      refetch();
+    } catch (error) {
+      alert("an error occured");
+
+      
+    }
+  };
+
   return (
     <div className="mt-8">
       <h3 className="text-lg font-medium mb-8">Leave a Comment</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <textarea
           name="text"
           value={comment}
@@ -16,9 +49,13 @@ const PostAComment = () => {
           placeholder="Share your opinion about this post"
           className="w-full bg-bgPrimary focus:outline-none p-5"
         ></textarea>
-        <button type="submit" className="w-full bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded">Submit</button>
+        <button
+          type="submit"
+          className="w-full bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded"
+        >
+          Submit
+        </button>
       </form>
-
     </div>
   );
 };
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,13 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { blogApi } from "./features/blogs/blogsApi";
 import authApi from "./features/auth/authApi";
 import authReducer from "./features/auth/authSlice";
+import commentApi from "./features/comments/commentApi";
 
 export const store = configureStore({
   reducer: {
     [blogApi.reducerPath]: blogApi.reducer, // Ensure `reducerPath` is unique
     [authApi.reducerPath]: authApi.reducer, // Ensure `reducerPath` is unique
+    [commentApi.reducerPath]:commentApi.reducer,
     auth: authReducer,
+
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(blogApi.middleware, authApi.middleware),
+    getDefaultMiddleware().concat(blogApi.middleware, authApi.middleware,commentApi.middleware),
 });
